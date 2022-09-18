0fbde0cbef60ad6f6716c627364062a31c64f017
AhmedMoneimm <102304282+AhmedMoneimm@users.noreply.github.com>
2022-09-18 23:10:36 +0200

    Update main.py

--- Task3/main.py
+++ Task3/main.py
@@ -2,6 +2,6 @@ import numpy as np
 import cv2
 
 img=cv2.imread('boat1.jpeg',0)
-img[img >=255] = 0  #the required threshold value (manual/static or make a fuction for it to be dynamic)
+img[img >=255] = 0  #the required threshold value (manual/static or make a fuction for it to be dynamic, like in auto canny in Task6) 
 
 cv2.imshow('NewImg',img);cv2.waitKey(0);cv2.destroyAllWindows()
